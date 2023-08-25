71b217f848970e2c9d88d5a4bcbe556c7b43bdc0
raulmasi <raul.martinez@ceiba.com.co>
2023-08-25 14:58:12 -0500

    Se agrega Tooltip a botones

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -14,6 +14,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatCardModule } from '@angular/material/card';
 import { LimiteCaracteresPipe } from './pipe/limite-caracteres.pipe';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 @NgModule({
   declarations: [
@@ -40,7 +41,8 @@ import { LimiteCaracteresPipe } from './pipe/limite-caracteres.pipe';
     MatSelectModule,
     MatButtonModule,
     MatIconModule,
-    MatCardModule
+    MatCardModule,
+    MatTooltipModule
   ]
 })
 export class SharedModule { }
